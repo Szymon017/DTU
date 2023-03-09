24cff31a908490b547007c85c993159ee661cd9a
Szymon Kacprzyk <szymon.kacprzyk5@gmail.com>
2023-03-09 23:37:31 +0100

    Persons fixed

--- client/src/components/Persons/Persons.js
+++ client/src/components/Persons/Persons.js
@@ -1,9 +1,33 @@
 import React from "react";
 import Container from "react-bootstrap/esm/Container";
 import { Row, Col, Button } from "react-bootstrap"
-import { useState } from "react";
+import { useState, useRef, useMemo } from "react";
+import JoditEditor from "jodit-react";
+import '../../style.css'
 const Persons = () => {
     const [register, setRegister] = useState(false)
+    const [personActive, setPersonActive] = useState(false)
+    const [desc, setDesc] = useState(false)
+    const [cases, setCases] = useState(false)
+    const [vehicles, setVehicles] = useState(false)
+    const [editPersonData, setEditPersonData] = useState(false)
+    const editor = useRef(null);
+    const [content, setContent] = useState('');
+    const [savedContent, setSavedContent] = useState('')
+
+    const handleChange = (con) => {
+        setContent(con)
+    }
+    const config = useMemo(() => ({
+        readonly: false,
+        theme: "dark",
+        height: "29rem",
+        allowResizeX: false,
+        allowResizeY: false,
+        colorPickerDefaultTab: 'color',
+    }), [])
+
+
     return (
         <>
             <Container className="defaultContainer" fluid>
@@ -112,49 +136,87 @@ const Persons = () => {
                             </Col>
                         </Row>
                     </Col>
-                    <Col style={{ "minHeight": "100%", }}>
-                        <Row>
-                            <Col style={{ "padding": "0.5rem" }}><img src="https://wallpapers.com/images/featured/hd-a5u9zq0a0ymy2dug.jpg" width="200px" /></Col>
-                            <Col style={{ 'textAlign': 'left', "padding": "0.5rem" }}>
-                                <Row>
-                                    <Col>Imie</Col><Col>John</Col>
-                                </Row><Row>
-                                    <Col>Nazwisko</Col><Col>Brown</Col>
-                                </Row><Row>
-                                    <Col>Numer dowodu</Col><Col>XD-0000</Col>
-                                </Row><Row>
-                                    <Col>Numer telefonu</Col><Col>12345</Col>
-                                </Row>
-                                <Row>
-                                    <Col>Adres e-mail</Col><Col>j.brown@gmail.com</Col>
-                                </Row>
-                                <Row>
-                                    <Col>Aktualne sprawy</Col>
-                                </Row>
-                            </Col>
-                        </Row>
-                        <Row >
-                        </Row>
-                        {register?(
-                            <><Row style={{"textAlign":"left"}}><Col><Button variant='warning'>Dodaj nową osobę</Button></Col></Row><Row className='editPersonData'><Col></Col><Col></Col></Row>
-                            <Row className='addNewPersonForm'>
-                            <Col>
-                                <form>
-                                    <label for="exampleInputEmail1">Imię</label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Imię" />
-                                    <label for="exampleInputEmail1">Nazwisko</label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Imię" />
-                                    <label for="exampleInputEmail1">Numer dowodu</label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Imię" />
-                                    <label for="exampleInputEmail1">Numer telefonu</label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Imię" />
-                                    <label for="exampleInputEmail1">Adres e-mail</label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Imię" />
-                                    <Button variant='dark'>Dodaj nową osobę</Button>
-                                </form>
-                            </Col>
-                        </Row></>
-                        ):(<><Row style={{"textAlign":"left"}}><Col><Button variant='warning'>Dodaj nową osobę</Button></Col></Row><Row className='editPersonData'><Col></Col><Col></Col></Row></>)}
+                    <Col style={{ "minHeight": "100%", "background-color": "#171d2a" }}>
+                        {register ? (
+                            <Row>
+                                <Col>Dodaj nową osobę</Col>
+                            </Row>
+                        ) : ""}
+                        {!personActive ? (
+                            <Row style={{ "height": "100%" }}>
+                                <Col style={{ "height": "100%" }}>
+                                    <Row>
+                                        <Col style={{ "padding": "0.5rem ", "background-color": "black" }}><img src="https://wallpapers.com/images/featured/hd-a5u9zq0a0ymy2dug.jpg" width="200px" /></Col>
+                                        <Col style={{ 'textAlign': 'left', "padding": "0.5rem", "background-color": "black" }}>
+                                            <Row>
+                                                <Col>Imie</Col><Col>John</Col>
+                                            </Row><Row>
+                                                <Col>Nazwisko</Col><Col>Brown</Col>
+                                            </Row><Row>
+                                                <Col>Numer dowodu</Col><Col>XD-0000</Col>
+                                            </Row><Row>
+                                                <Col>Numer telefonu</Col><Col>12345</Col>
+                                            </Row>
+                                            <Row>
+                                                <Col>Adres e-mail</Col><Col>j.brown@gmail.com</Col>
+                                            </Row>
+
+                                        </Col>
+                                    </Row>
+                                    <Row style={{ "margin": "0.5rem", "height": "100%" }}>
+                                        <Col className="personsMiniMenu">
+                                            {(!desc && !editPersonData) ? (
+
+                                                <>
+                                                    <Row><Col className="personsMiniMenuItem" onClick={() => { setDesc(!desc) }}>Opis</Col></Row>
+                                                    <Row><Col className="personsMiniMenuItem" onClick={() => { setCases(!cases) }}>Powiązane sprawy</Col></Row>
+                                                    <Row><Col className="personsMiniMenuItem" onClick={() => { setVehicles(!vehicles) }}>Pojazdy</Col></Row>
+                                                </>
+                                            ) : ""
+                                            }{desc ? (
+                                                <>
+                                                    <Row><Col className="personsMiniMenuItem" onClick={() => { setEditPersonData(true); setCases(false); setVehicles(false); setDesc(false) }}>Edytuj opis</Col></Row>
+                                                    <Row><Col className="personsMiniMenuItem" onClick={() => { setCases(!cases); setDesc(false) }}>Powiązane sprawy</Col></Row>
+                                                    <Row><Col className="personsMiniMenuItem" onClick={() => { setVehicles(!vehicles); setDesc(false) }}>Pojazdy</Col></Row>
+                                                </>
+                                            ) : ""
+                                            }{editPersonData ? (
+                                                <>
+                                                    <Row><Col className="personsMiniMenuItem" onClick={() => { setEditPersonData(false); setCases(true); setVehicles(true); setDesc(true) }}>Zapisz</Col></Row>
+                                                    <Row><Col className="personsMiniMenuItem" onClick={() => { setEditPersonData(false); setCases(true); setVehicles(true); setDesc(true) }}>Anuluj</Col></Row>
+                                                </>
+                                            ) : ("")
+
+                                            }
+                                        </Col>
+                                        <Col>
+                                            <Row style={{ "height": "75%" }}>
+                                                <Col className="personDescription">
+                                                    {editPersonData ? (
+                                                            <>
+                                                                 <JoditEditor
+                                        ref={editor}
+                                        value={content}
+                                        config={config}
+                                        onChange={newContent => setContent(newContent)} />
+                                                            </>
+                                                    ) : (<>
+                                                        {
+                                                            desc?(<div dangerouslySetInnerHTML={{ __html: content }} />):""
+                                                        }
+                                                    </>)}
+                                                    
+                                                </Col>
+                                            </Row>
+
+                                        </Col>
+                                    </Row>
+
+                                </Col>
+                            </Row>
+
+
+                        ) : ""}
                     </Col>
                 </Row>
 
@@ -164,32 +226,32 @@ const Persons = () => {
     /*
  
 <Row className='singleInformantMenu'>
-      <Col>Imie</Col>
-      <Col>Nazwisko</Col>
-      <Col>Numer</Col>
-      <Col>E-mail</Col>
-      <Col>Powiązane sprawy</Col>
-      <Col>Numer dowodu</Col>
-  </Row>
+<Col>Imie</Col>
+<Col>Nazwisko</Col>
+<Col>Numer</Col>
+<Col>E-mail</Col>
+<Col>Powiązane sprawy</Col>
+<Col>Numer dowodu</Col>
+</Row>
 <Row className='informantsTable' style={{"overflow-y":"auto", "maxHeight":"620px"}}>
-  <Col>
-  
-  <Row className='singleInformant'>
-      <Col>John</Col>
-      <Col>Brown</Col>
-      <Col>12312</Col>
-      <Col>johnbrown@gmail.com</Col>
-      <Col><u>link</u></Col>
-      <Col>XD-6969</Col>
-  </Row>
-  <Row className='singleInformant'>
-      <Col>John</Col>
-      <Col>Brown</Col>
-      <Col>12312</Col>
-      <Col>johnbrown@gmail.com</Col>
-      <Col><u>link</u></Col>
-      <Col>XD-6969</Col>
-  </Row>
+<Col>
+ 
+<Row className='singleInformant'>
+<Col>John</Col>
+<Col>Brown</Col>
+<Col>12312</Col>
+<Col>johnbrown@gmail.com</Col>
+<Col><u>link</u></Col>
+<Col>XD-6969</Col>
+</Row>
+<Row className='singleInformant'>
+<Col>John</Col>
+<Col>Brown</Col>
+<Col>12312</Col>
+<Col>johnbrown@gmail.com</Col>
+<Col><u>link</u></Col>
+<Col>XD-6969</Col>
+</Row>
  
 */
 }
