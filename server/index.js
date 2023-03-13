ec12f0db16889cb2098e62a20b65defa545c4709
Szymon Kacprzyk <szymon.kacprzyk5@gmail.com>
2023-03-14 00:03:35 +0100

    Setting up a server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = Number(process.env.PORT) ;
 
 app.listen(PORT, (err) => {
     console.log(`Server running on port ${PORT}`);
