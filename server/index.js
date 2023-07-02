7ba1cc999ff983119ab3517c6539afaf7d43e1aa
Szymon Kacprzyk <szymon.kacprzyk5@gmail.com>
2023-07-02 20:30:22 +0200

    Deploying app

--- server/index.js
+++ server/index.js
@@ -35,9 +35,9 @@ try{
 
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+  res.sendFile(path.join(__dirname, "/client/build/index.html"))
 );
 
 app.use('/officers', Officers)
