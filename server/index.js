2f7787b47b27515fc8c8b913d2634623812126c5
Szymon Kacprzyk <szymon.kacprzyk5@gmail.com>
2023-07-02 22:55:47 +0200

    Deploying app

--- server/index.js
+++ server/index.js
@@ -19,10 +19,10 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use(express.json());
-app.use(cors({
-    origin: true,
-    credentials: true
-}));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/client/build")));
