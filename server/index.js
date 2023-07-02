626d1c80960586ec99ef46a76c9f8d418595f9c6
Szymon Kacprzyk <szymon.kacprzyk5@gmail.com>
2023-07-02 22:41:36 +0200

    Deploying app

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use(express.json());
 app.use(cors({
-    origin: ['https://detective-task-unit-sa.onrender.com/']
+    origin: true
 }));
 
 const __dirname = path.resolve();
